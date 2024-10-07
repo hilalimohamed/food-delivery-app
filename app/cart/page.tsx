61b2589bafa20952cceeb6498b15c3c71e99a851
hilalimohamed <125507958+hilalimohamed@users.noreply.github.com>
2024-10-07 13:17:47 +0100

    update

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
-import CartPage from "./components/CartPage";
+// import CartPage from "./components/CartPage";
+
+import dynamic from "next/dynamic";
+
+// Dynamic import of CartPage
+const CartPage = dynamic(() => import("./components/CartPage"), {
+  ssr: false,
+});
 
 export default function Page() {
   return (

a399716b5adc4ea947fda27ae8b7b1e959c887a3
hilalimohamed <125507958+hilalimohamed@users.noreply.github.com>
2024-10-20 23:44:50 +0100

    update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -4,7 +4,7 @@ import getUser from '../action/getUser';
 
 async function page() {
   const getuser = await getUser()
-  console.log('chof hna lfo9   :   ',getuser)
+  // console.log('chof hna lfo9   :   ',getuser)
   return (
     <div>
       <ProfilePage user={getuser} />
